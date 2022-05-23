a48365f2a174568d5179fb44ad0a1c787d4609e3
felipe <felipebaloneker.com>
2022-05-23 13:01:39 -0300

    fix useauth import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton"
 import Profile from "./components/Profile";
-import {userAuth0} from '@auth0/auth0-react'
+import {useAuth0} from '@auth0/auth0-react'
 
 function App() {
-  const {isLoading} = userAuth0();
+  const {isLoading} = useAuth0();
   
   if(isLoading) return <div>Loading...</div>
 
