269a6b15dcebc4c0fc2fea67c9eb9947fb6d07a6
felipe <felipebaloneker.com>
2022-05-23 13:00:37 -0300

    added loading in user authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton"
 import Profile from "./components/Profile";
+import {userAuth0} from '@auth0/auth0-react'
 
 function App() {
+  const {isLoading} = userAuth0();
+  
+  if(isLoading) return <div>Loading...</div>
+
   return (
     <>
       <LoginButton/>
